1e10ddee02772146e30413340509a67e2eba1d7a
DemonGT <123163603+VicenziPrst@users.noreply.github.com>
2023-02-05 14:02:12 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,16 +4,16 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+let str = `Type .discord For Info Sc Bot`
     let waifu = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/waifu.txt')
     let chen = await waifu.text()
     let ran = chen.split('\n')
     let yae = ran[Math.floor(Math.random() * ran.length)]
     let thumb = await(await fetch(yae)).buffer()
-conn.sendButtonDoc(m.chat, str, wm, 'ᴡᴀɴᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ?', 'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.sendButtonDoc(m.chat, str, wm, '100+ SUBS SC UP', 'Discord','.discord', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: global.sig,
     title: wm3,
-    body: 'want source code?',
+    body: 'Sc Up Pas 100+ Subs',
     thumbnail: thumb,
     sourceUrl: sig
   }
